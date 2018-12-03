18f740232d1344c394e27a1813ab4ff417d53dce
Beta-TNT <zhaoyu243@sohu.com>
2018-12-03 11:51:49 +0800

    Flag peek func added

--- AnalyseLib.py
+++ AnalyseLib.py
@@ -133,11 +133,10 @@ class AnalyseBase(object):
     _cache = dict() # Flag-缓存对象字典
     _timer = dict() # Flag-计时器对象字典
 
-    def __init__(self, InputRules):
+    def __init__(self, InputRules = None):
         if type(InputRules) != list:
             raise TypeError('Invalid InputRules type, expecting list')
         self._rules = InputRules    # 规则列表
-        self._cache = dict()        # 缓存
 
     def __RemoveFlag(self, InputFlag):
         if InputFlag in self._cache:
@@ -155,6 +154,14 @@ class AnalyseBase(object):
                 self._cache.pop(InputFlag)
             hitItem = None if rtn == False else hitItem
         return rtn, hitItem
+
+    def _DefaultFlagPeek(self, InputFlag):
+        '默认Flag偷窥函数，检查Flag是否有效。返回定义和默认Flag检查函数不同，但并不会触发Threhold或Lifetime消耗，也不进行缓存管理'
+        rtn = False
+        hitItem = self._cache.get(InputFlag, None)
+        if hitItem:
+            rtn = hitItem.Valid and not bool(hitItem.ThreholdRemain)
+        return rtn, None if not rtn else hitItem
 
     def _DefaultFieldCheck(self, TargetData, InputFieldCheckRule):
         '默认的字段检查函数，输入字段的内容以及单条字段检查规则，返回True/False'
@@ -196,7 +203,8 @@ class AnalyseBase(object):
             fieldCheckResult = not fieldCheckResult
         return fieldCheckResult
 
-    def _DefaultFlagGenerator(self, InputData, InputTemplate, BytesDecoding='utf-16'):
+    @staticmethod
+    def _DefaultFlagGenerator(InputData, InputTemplate, BytesDecoding='utf-16'):
         '默认的Flag生成函数，根据输入的数据和模板构造Flag。将模板里用大括号包起来的字段名替换为InputData对应字段的内容，如果包含bytes字段，需要指定解码方法'
         if InputTemplate == None:
             return None
@@ -231,10 +239,10 @@ class AnalyseBase(object):
                 "Invalid InputData or InputRule type, expecting dict")
 
         fieldCheckResult = False
-        if InputRule["FieldCheckList"]:
+        if type(InputRule["FieldCheckList"]) == dict:
             # 字段检查遍历循环，用字段检查规则轮数据
             for fieldChecker in InputRule["FieldCheckList"]:
-                if fieldChecker["FieldName"] in InputData:
+                if fieldChecker.get("FieldName") in InputData:
                     targetData = InputData.get(fieldChecker["FieldName"])
                     fieldCheckResult = self.FieldCheck(
                         targetData, fieldChecker)
@@ -279,10 +287,15 @@ class AnalyseBase(object):
         'Flag检查函数，可根据需要在派生类里重写。应返回一个二元Tuple，分别是Flag是否有效，以及有效的Flag命中的对象。没有命中返回(False, None)'
         return self._DefaultFlagCheck(InputFlag)
 
-    def FlagGenerator(self, InputData, InputTemplate):
+    def FlagPeek(self, InputFlag):
+        'Flag偷看函数，可根据需要在派生类里重写。返回值定义和FlagCheck相同，但并不会消耗Threhold或Lifetime，也不进行缓存管理'
+        return self._DefaultFlagPeek(InputFlag)
+
+    @staticmethod
+    def FlagGenerator(InputData, InputTemplate):
         'Flag生成函数，可根据需要在派生类里重写'
         'Flag Generator func, you may overwrite it in child class if necessary.'
-        return self._DefaultFlagGenerator(InputData, InputTemplate)
+        return AnalyseBase._DefaultFlagGenerator(InputData, InputTemplate)
 
     def SingleRuleTest(self, InputData, InputRule):
         '单规则匹配函数，可根据需要在派生类里重写'
@@ -321,8 +334,7 @@ class AnalyseBase(object):
                 # 1、构造本级Flag；   Generate current flag;
                 # 2、调用ActionFunc()获得用户数据，构造CacheItem对象；  Call ActionFunc() to get a user defined data
                 # 3、以本级Flag作为Key，新的CacheItem作为Value，存入self._cache[]； Save cache item into self._cache[], with current flag as key
-                currentFlag = self.FlagGenerator(
-                    InputData, rule["CurrentFlag"])
+                currentFlag = self.FlagGenerator(InputData, rule["CurrentFlag"])
                 newDataItem = ActionFunc(InputData, rule, hitItem, currentFlag)
                 if currentFlag not in self._cache and currentFlag != None:
                     # 将命中规则的数据、规则本身、命中的缓存对象以及命中的Flag传给用户函数，获得用户函数返回值
